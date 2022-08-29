094ca60d8dd27edd22ff704dd4627ef18e8c2b75
Олег <nexxxt.07@gmail.com>
2022-08-29 14:22:30 +0300

    29.08 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    const navs = document.querySelectorAll('#navTop a');
+    navs.forEach(el => {
+        el.addEventListener('click', () => {
+            var myCollapse = document.getElementById('collapseMenu');
+            var collapse = bootstrap.Collapse.getInstance(myCollapse);
+            collapse.toggle();
+            return true;
+        });
+    });
+
 });
